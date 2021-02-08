0015ea58bbfa8e18f7a913c674a63e482bb05661
daria nikanorova <daria.nikanorova@mail.ru>
2021-02-08 12:23:30 +0300

    Added measurements for np.random with size argument

--- 1_np_random_time.py
+++ 1_np_random_time.py
@@ -5,28 +5,36 @@ import matplotlib.pyplot as plt
 
 
 def np_random_time(n):
-    np_result, random_result = [], []
+    np_result, random_result, np_size_result = [], [], []
     up = 1
     while up <= n:
-        start = time.time()
+        print(up)
+        start1 = time.time()
+        np.random.uniform(0, 1, size=up)
+        end1 = time.time()
+        np_size_time = end1 - start1
+        np_size_result += [np_size_time]
+        start2 = time.time()
         for i in range(0, up):
             np.random.uniform(0, 1)
-        end = time.time()
-        np_time = end - start
+        end2 = time.time()
+        np_time = end2 - start2
         np_result += [np_time]
-        start = time.time()
+        start3 = time.time()
         for j in range(0, up):
             random.uniform(0, 1)
-        end = time.time()
-        random_time = end - start
+        end3 = time.time()
+        random_time = end3 - start3
         random_result += [random_time]
         up *= 10
-    return np_result, random_result
+    return np_result, random_result, np_size_result
 
 
-res = np_random_time(100000)
+res = np_random_time(10000)
 np_res = res[0]
+print(np_res)
 random_res = res[1]
+np_size_res = res[2]
 xs = [10 ** i for i in range(len(np_res))]
 
 # image size
@@ -37,6 +45,7 @@ plt.rcParams['font.size'] = 15
 # labels are needed for a legend
 plt.plot(xs, np_res, label='numpy random')
 plt.plot(xs, random_res, label='random')
+plt.plot(xs, np_size_res, label='numpy random (with size parameter)')
 
 plt.title('Numpy random vs random')
 
@@ -50,4 +59,5 @@ plt.legend()
 # add grid
 plt.grid()
 
+# plt.show()
 plt.savefig('1_np_random_time.png')
