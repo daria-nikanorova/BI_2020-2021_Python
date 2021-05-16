c8cd0c1e133af9fc57a49b0da67f8dd06573f2d6
daria nikanorova <daria.nikanorova@mail.ru>
2021-05-16 20:47:40 +0300

    Fixed bugs

--- DNA_RNA_class.py
+++ DNA_RNA_class.py
@@ -1,24 +1,28 @@
 class Rna:
+
     def __init__(self, seq: str):
-        self.seq = seq.upper()
-        for base in self.seq:
-            if base not in ['A', 'U', 'C', 'G']:
-                raise ValueError(f'Ambiguous base "{base}" was found, only A, U, G or C are expected.\n'
-                                 f'Please, check your RNA sequence and try again')
+        self.seq = seq
+        if len(self.seq) > 0:
+            for base in self.seq.upper():
+                if base not in ['A', 'U', 'C', 'G']:
+                    raise ValueError(f'Ambiguous base "{base}" was found, only A, U, G or C are expected.\n'
+                                     f'Please, check your RNA sequence and try again')
+        else:
+            raise ValueError('An RNA sequence must be of positive length.\n'
+                             'Please, check your RNA sequence and try again')
 
     def __iter__(self):  # as far as string is iterable, we can use its iter, rather than write special iterator
         return iter(self.seq)
 
     def __hash__(self):
-        return hash(self.seq)
+        return hash(tuple(self.seq))
 
     def __eq__(self, other):  # two instances are equal if their sequences are equal
-        if isinstance(other, Rna):
-            return self.seq == other.seq
-        return False
+        return isinstance(other, Rna) and self.seq == other.seq
 
     def gc_content(self):
-        return (self.seq.count('G') + self.seq.count('C')) * 100 / len(self.seq)
+        return (self.seq.upper().count('G') + self.seq.upper().count('C')) * 100 / len(
+            self.seq)
 
     def reverse_complement(self):
         complementary_bases = {"A": "U",
@@ -26,32 +30,35 @@ class Rna:
                                "G": "C",
                                "C": "G"}
         rev_comp_seq = str()
-        for base in self.seq[::-1]:
+        for base in self.seq[::-1].upper():
             rev_comp_seq += complementary_bases[base]
-        return Rna(rev_comp_seq) # this method returns new instance of Rna() class
+        return Rna(rev_comp_seq)  # this method returns new instance of Rna() class
 
 
 class Dna:
+
     def __init__(self, seq: str):
-        self.seq = seq.upper()
-        for base in self.seq:
-            if base not in ['A', 'T', 'C', 'G']:
-                raise ValueError(f'Ambiguous base "{base}" was found, only A, T, G or C are expected.\n'
-                                 f'Please, check your DNA sequence and try again')
+        self.seq = seq
+        if len(self.seq) > 0:
+            for base in self.seq.upper():
+                if base not in ['A', 'T', 'C', 'G']:
+                    raise ValueError(f'Ambiguous base "{base}" was found, only A, T, G or C are expected.\n'
+                                     f'Please, check your DNA sequence and try again')
+        else:
+            raise ValueError('A DNA sequence must be of positive length.\n'
+                             'Please, check your DNA sequence and try again')
 
     def __iter__(self):
         return iter(self.seq)
 
     def __hash__(self):
-        return hash(self.seq)
+        return hash(tuple(self.seq))
 
     def __eq__(self, other):
-        if isinstance(other, Dna):
-            return self.seq == other.seq
-        return False
+        return isinstance(other, Dna) and self.seq == other.seq
 
     def gc_content(self):
-        return (self.seq.count('G') + self.seq.count('C')) * 100 / len(self.seq)
+        return (self.seq.upper().count('G') + self.seq.upper().count('C')) * 100 / len(self.seq)
 
     def reverse_complement(self):
         complementary_bases = {"A": "T",
@@ -59,9 +66,9 @@ class Dna:
                                "G": "C",
                                "C": "G"}
         rev_comp_seq = str()
-        for base in self.seq[::-1]:
+        for base in self.seq[::-1].upper():
             rev_comp_seq += complementary_bases[base]
-        return Dna(rev_comp_seq) # this method returns new instance of Dna() class
+        return Dna(rev_comp_seq)  # this method returns new instance of Dna() class
 
     def transcribe(self):
         complementary_bases_transcription = {"A": "U",
@@ -69,9 +76,9 @@ class Dna:
                                              "G": "C",
                                              "C": "G"}
         transcript = str()
-        for base in self.seq:
+        for base in self.seq.upper():
             transcript += complementary_bases_transcription[base]
-        return Rna(transcript) # this method returns new instance of Rna() class
+        return Rna(transcript)  # this method returns new instance of Rna() class
 
 
 # SUPPLEMENTARY
